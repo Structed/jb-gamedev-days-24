9ab0daf0b4f4d4438c4cfc784c16f59a464d794d
Johannes Ebner <Structed@users.noreply.github.com>
2024-10-05 17:07:16 +0200

    Initial implementation

--- addons/demo_guide/demo_guide.gd
+++ addons/demo_guide/demo_guide.gd
@@ -1,12 +1,41 @@
 @tool
 extends EditorPlugin
 
+const MainPanel : PackedScene = preload("res://addons/demo_guide/Scenes/MainScene.tscn")
 
-func _enter_tree():
+var main_panel_instance
+
+func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	pass
+	main_panel_instance = MainPanel.instantiate()
+	# Add the main panel to the editor's main viewport.
+	get_editor_interface().get_editor_main_screen().add_child(main_panel_instance)
+	# Hide the main panel. Very much required.
+	_make_visible(false)
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	pass
+	if main_panel_instance:
+		main_panel_instance.queue_free()
+
+
+# Determine's whether this is a main screen panel that goes next to the main viewport change buttons
+func _has_main_screen() -> bool:
+	return true
+
+
+# Main screen plugins need to be hidden by default
+func _make_visible(visible) -> void:
+	if main_panel_instance:
+		main_panel_instance.visible = visible
+
+
+# Customize the text displayed on the button
+func _get_plugin_name() -> String:
+	return "Demo Guide"
+
+
+# Customize the icon shown on the button
+func _get_plugin_icon() -> Texture2D:
+	return load("res://addons/demo_guide/Godot_icon.png")
